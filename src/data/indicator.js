a71c2587ba3c48a1da7105cec30d4cf6876c062d
admin <mailhendra2@gmail.com>
2018-08-18 01:16:29 +0700

    add data

--- src/data/indicator.js
+++ src/data/indicator.js
@@ -2,22 +2,71 @@ let indicator = [{
     id:1,
     list:[{
             id:1,
-            desc:"1.1.1 - Propotion of popular extreme poverty for all people everywhere currently measured as people living less than $1.25 a day"
+            desc:"1.1.1: Proportion of population below the international poverty line, by sex, age, employment status and geographical location (urban/rural)"
           },
           {
             id:2,
-            desc:"1.2.1 -Propotion of popular reduce at least by half"
-          },]
+            desc:"1.2.1: Proportion of population living below the national poverty line, by sex and age"
+          },
+          {
+            id:3,
+            desc:"1.2.2: Proportion of men, women and children of all ages living in poverty in all its dimensions according to national definitions"
+          },
+          {
+            id:4,
+            desc:"1.3.1: Proportion of population covered by social protection floors/systems, by sex, distinguishing children, unemployed persons, older persons, persons with disabilities, pregnant women, newborns, work-injury victims and the poor and the vulnerable"
+          },
+          {
+            id:5,
+            desc:"1.4.1: Proportion of population living in households with access to basic services"
+          },
+          {
+            id:6,
+            desc:"1.4.2: Proportion of total adult population with secure tenure rights to land, with legally recognized documentation and who perceive their rights to land as secure, by sex and by type of tenure"
+          },
+          {
+            id:7,
+            desc:"1.5.1: Number of deaths, missing persons and directly affected persons attributed to disasters per 100,000 population"
+          },
+          {
+            id:8,
+            desc:"1.5.2: Direct economic loss attributed to disasters in relation to global gross domestic product (GDP)"
+          },
+          {
+            id:9,
+            desc:"1.5.3: Number of countries that adopt and implement national disaster risk reduction strategies in line with the Sendai Framework for Disaster Risk Reduction 2015-2030"
+          },
+          {
+            id:10,
+            desc:"1.5.4: Proportion of local governments that adopt and implement local disaster risk reduction strategies in line with national disaster risk reduction strategies"
+          },
+          {
+            id:11,
+            desc:"1.a.1: Proportion of domestically generated resources allocated by the government directly to poverty reduction programmes"
+          },
+          {
+            id:12,
+            desc:"1.a.2: Proportion of total government spending on essential services (education, health and social protection)"
+          },
+          {
+            id:13,
+            desc:"1.a.3: Sum of total grants and non-debt-creating inflows directly allocated to poverty reduction programmes as a proportion of GDP"
+          },
+          {
+            id:14,
+            desc:"1.b.1: Proportion of government recurrent and capital spending to sectors that disproportionately benefit women, the poor and vulnerable groups"
+          }
+        ]
   },
   {
     id:2,
     list:[{
             id:1,
-            desc:"2.2.1 - By 2030 eradicate extreme poverty for all people everywhere currently measured as people living less than $1.25 a day"
+            desc:"2.1.1: Prevalence of undernourishment"
           },
           {
             id:2,
-            desc:"2.2.2 - By 2030 reduce at least by half"
+            desc:"2.1.2: Prevalence of moderate or severe food insecurity in the population, based on the Food Insecurity Experience Scale (FIES)"
           },]
   }]
 
--- src/data/target.js
+++ src/data/target.js
@@ -6,19 +6,63 @@ let target = [{
                       },
                       {
                         id:2,
-                        desc:"1.2 - By 2030 reduce at least by half"
-                      },]
+                        desc:"1.2 - By 2030, reduce at least by half the proportion of men, women and children of all ages living in poverty in all its dimensions according to national definitions"
+                      },
+                      {
+                        id:3,
+                        desc:"1.3 - Implement nationally appropriate social protection systems and measures for all, including floors, and by 2030 achieve substantial coverage of the poor and the vulnerable"
+                      },
+                      {
+                        id:4,
+                        desc:"1.4 - By 2030, ensure that all men and women, in particular the poor and the vulnerable, have equal rights to economic resources, as well as access to basic services, ownership and control over land and other forms of property, inheritance, natural resources, appropriate new technology and financial services, including microfinance"
+                      },
+                      {
+                        id:5,
+                        desc:"1.5 - By 2030, build the resilience of the poor and those in vulnerable situations and reduce their exposure and vulnerability to climate-related extreme events and other economic, social and environmental shocks and disasters"
+                      },
+                      {
+                        id:6,
+                        desc:"1.a - Ensure significant mobilization of resources from a variety of sources, including through enhanced development cooperation, in order to provide adequate and predictable means for developing countries, in particular least developed countries, to implement programmes and policies to end poverty in all its dimensions"
+                      },
+                      {
+                        id:7,
+                        desc:"1.b - Create sound policy frameworks at the national, regional and international levels, based on pro-poor and gender-sensitive development strategies, to support accelerated investment in poverty eradication actions"
+                      }]
               },
               {
                 id:2,
                 list:[{
                         id:1,
-                        desc:"2.1 - By 2030 eradicate extreme poverty for all people everywhere currently measured as people living less than $1.25 a day"
+                        desc:" 2.1 - By 2030, end hunger and ensure access by all people, in particular the poor and people in vulnerable situations, including infants, to safe, nutritious and sufficient food all year round"
                       },
                       {
                         id:2,
-                        desc:"2.2 - By 2030 reduce at least by half"
-                      },]
+                        desc:"2.2 - By 2030, end all forms of malnutrition, including achieving, by 2025, the internationally agreed targets on stunting and wasting in children under 5 years of age, and address the nutritional needs of adolescent girls, pregnant and lactating women and older persons"
+                      },
+                      {
+                        id:3,
+                        desc:"2.3 - By 2030, double the agricultural productivity and incomes of small-scale food producers, in particular women, indigenous peoples, family farmers, pastoralists and fishers, including through secure and equal access to land, other productive resources and inputs, knowledge, financial services, markets and opportunities for value addition and non-farm employment"
+                      },
+                      {
+                        id:4,
+                        desc:"2.4 - By 2030, ensure sustainable food production systems and implement resilient agricultural practices that increase productivity and production, that help maintain ecosystems, that strengthen capacity for adaptation to climate change, extreme weather, drought, flooding and other disasters and that progressively improve land and soil quality"
+                      },
+                      {
+                        id:5,
+                        desc:"2.5 - By 2020, maintain the genetic diversity of seeds, cultivated plants and farmed and domesticated animals and their related wild species, including through soundly managed and diversified seed and plant banks at the national, regional and international levels, and promote access to and fair and equitable sharing of benefits arising from the utilization of genetic resources and associated traditional knowledge, as internationally agreed"
+                      },
+                      {
+                        id:6,
+                        desc:"2.a - Increase investment, including through enhanced international cooperation, in rural infrastructure, agricultural research and extension services, technology development and plant and livestock gene banks in order to enhance agricultural productive capacity in developing countries, in particular least developed countries"
+                      },
+                      {
+                        id:7,
+                        desc:"2.b - Correct and prevent trade restrictions and distortions in world agricultural markets, including through the parallel elimination of all forms of agricultural export subsidies and all export measures with equivalent effect, in accordance with the mandate of the Doha Development Round"
+                      },
+                      {
+                        id:8,
+                        desc:"2.c - Adopt measures to ensure the proper functioning of food commodity markets and their derivatives and facilitate timely access to market information, including on food reserves, in order to help limit extreme food price volatility"
+                      }]
               }]
 
 export default target;
