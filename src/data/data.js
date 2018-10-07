f13ded5436d006a74c1ea59016b9a937e4502ee3
admin <mailhendra2@gmail.com>
2018-10-07 22:14:30 +0700

    update kata

--- src/data/data.js
+++ src/data/data.js
@@ -3,14 +3,14 @@ let list = [{
                 title:'TANPA KEMISKINAN',
                 subtitle:'Mengakhiri Kemiskinan dalam Segala Bentuk Dimanapun',
                 color:'#E5243B',
-                sdgs_icon:'assets/imgs/sdgs1.jpg'
+                sdgc_icon:'assets/imgs/sdgs1.jpg'
             },
             {
                 id:2,
                 title:'TANPA KELAPARAN',
                 subtitle:'Menghilangkan Kelaparan, Mencapai Ketahanan Pangan dan Gizi yang Baik, serta Meningkatkan Pertanian Berkelanjutan',
                 color:'#DDA63A',
-                sdgs_icon:'assets/imgs/sdgs2.jpg'
+                sdgc_icon:'assets/imgs/sdgs2.jpg'
 
             },
             {
@@ -18,112 +18,112 @@ let list = [{
                 title:'KEHIDUPAN SEHAT DAN SEJAHTERA',
                 subtitle:'Menjamin Kehidupan yang Sehat dan Meningkatkan Kesejahteraan Seluruh Penduduk Semua Usia',
                 color:'#4C9F38',
-                sdgs_icon:'assets/imgs/sdgs3.jpg'
+                sdgc_icon:'assets/imgs/sdgs3.jpg'
             },
             {
                 id:4,
                 title:'PENDIDIKAN BERKUALITAS',
                 subtitle:'Menjamin Kualitas Pendidikan yang Inklusif dan Merata serta Meningkatkan Kesempatan Belajar Sepanjang Hayat untuk Semua',
                 color:'#C5192D',
-                sdgs_icon:'assets/imgs/sdgs4.jpg'
+                sdgc_icon:'assets/imgs/sdgs4.jpg'
             },
             {
                 id:5,
                 title:'KESETARAAN GENDER',
                 subtitle:'Mencapai Kesetaraan Gender dan Memberdayakan Perempuan',
                 color:'#FF3A21',
-                sdgs_icon:'assets/imgs/sdgs5.jpg'
+                sdgc_icon:'assets/imgs/sdgs5.jpg'
             },
             {
                 id:6,
                 title:'AIR BERSIH DAN SANITASI LAYAK',
                 subtitle:'Menjamin Ketersediaan serta Pengelolaan Air Bersih dan Sanitasi yang Berkelanjutan untuk Semu',
                 color:'#26BDE2',
-                sdgs_icon:'assets/imgs/sdgs6.jpg'
+                sdgc_icon:'assets/imgs/sdgs6.jpg'
             },
             {
                 id:7,
                 title:'ENERGI BERSIH DAN TERJANGKAU',
                 subtitle:'Menjamin Akses Energi yang Terjangkau, Andal, Berkelanjutan dan Modern untuk Semua',
                 color:'#FCC30B',
-                sdgs_icon:'assets/imgs/sdgs7.jpg'
+                sdgc_icon:'assets/imgs/sdgs7.jpg'
             },
             {
                 id:8,
                 title:'PEKERJAAN LAYAK DAN PERTUMBUHAN EKONOMI',
                 subtitle:'Meningkatkan Pertumbuhan Ekonomi yang Inklusif dan Berkelanjutan, Kesempatan Kerja yang Produktif dan Menyeluruh, serta Pekerjaan yang Layak untuk Semua',
                 color:'#A21942',
-                sdgs_icon:'assets/imgs/sdgs8.jpg'
+                sdgc_icon:'assets/imgs/sdgs8.jpg'
             },
             {
                 id:9,
                 title:'INDUSTRI, INOVASI DAN INFRASTRUKTUR',
                 subtitle:'Membangun Infrastruktur yang Tangguh, Meningkatkan Industri Inklusif dan Berkelanjutan, serta Mendorong Inovasi',
                 color:'#FD6925',
-                sdgs_icon:'assets/imgs/sdgs9.jpg'
+                sdgc_icon:'assets/imgs/sdgs9.jpg'
             },
             {
                 id:10,
                 title:'BERKURANGNYA KESENJANGAN',
                 subtitle:'Mengurangi Kesenjangan Intra- dan Antarnegara',
                 color:'#E11484',
-                sdgs_icon:'assets/imgs/sdgs10.jpg'
+                sdgc_icon:'assets/imgs/sdgs10.jpg'
             },
             {
                 id:11,
                 title:'KOTA DAN PEMUKIMAN YANG BERKELANJUTAN',
                 subtitle:'Menjadikan Kota dan Pemukiman Inklusif, Aman, Tannguh dan Berkelanjutan',
                 color:'#FD9D24',
-                sdgs_icon:'assets/imgs/sdgs11.jpg'
+                sdgc_icon:'assets/imgs/sdgs11.jpg'
             },
             {
                 id:12,
                 title:'KONSUMSI DAN PRODUKSI YANG BERTANGGUNG JAWAB',
                 subtitle:'Menjamin Pola Produksi dan Konsumsi yang Berkelanjutan',
                 color:'#BF8B2E',
-                sdgs_icon:'assets/imgs/sdgs12.jpg'
+                sdgc_icon:'assets/imgs/sdgs12.jpg'
             },
             {
                 id:13,
                 title:'PENANGANAN PERUBAHAN IKLIM',
                 subtitle:'Mengambil Tindakan Cepat untuk Mengatasi Perubahan Iklim dan Dampaknya',
                 color:'#3F7E44',
-                sdgs_icon:'assets/imgs/sdgs13.jpg'
+                sdgc_icon:'assets/imgs/sdgs13.jpg'
             },
             {
                 id:14,
                 title:'EKOSISTEM LAUTAN',
                 subtitle:'Melestarikan dan Memanfaatkan secara Berkelanjutan Sumber Daya Kelautan dan Samudera untuk Pembangunan Berkelanjutan',
                 color:'#0A97D9',
-                sdgs_icon:'assets/imgs/sdgs14.jpg'
+                sdgc_icon:'assets/imgs/sdgs14.jpg'
             },
             {
                 id:15,
                 title:'EKOSISTEM DARATAN',
-                subtitle:'Melestarikan dan Memanfaatkan secara Berkelanjutan Sumber Daya Kelautan dan Samudera untuk Pembangunan Berkelanjutan',
+                subtitle:'Melindungi, Merestorasi dan Meningkatkan Pemanfaatan Berkelanjutan Ekosistem Daratan, Mengelola Hutan Secara Lestari, Menghentikan Penggurunan, Memulihkan Degradasi lahan, serta Menghentikan Kehilangan Keanekaragaman Hayati',
                 color:'#56C02B',
-                sdgs_icon:'assets/imgs/sdgs15.jpg'
+                sdgc_icon:'assets/imgs/sdgs15.jpg'
             },
             {
                 id:16,
                 title:'PERDAMAIAN KEADILAN  DAN KELEMBAGAAN YANG TANGGUH',
-                subtitle:'Melestarikan dan Memanfaatkan secara Berkelanjutan Sumber Daya Kelautan dan Samudera untuk Pembangunan Berkelanjutan',
+                subtitle:'Perdamaian, stabilitas, hak-hak asasi manusia dan pemerintahan efektif berdasarkan aturan hukum adalah jalur pentimg menuju pembangunan berkelanjutan',
                 color:'#00689D',
-                sdgs_icon:'assets/imgs/sdgs16.jpg'
+                sdgc_icon:'assets/imgs/sdgs16.jpg'
             },
             {
                 id:17,
                 title:'KEMITRAAN UNTUK MENCAPAI TUJUAN',
-                subtitle:'Melestarikan dan Memanfaatkan secara Berkelanjutan Sumber Daya Kelautan dan Samudera untuk Pembangunan Berkelanjutan',
+                subtitle:'Menguatkan Sarana Pelaksanaan dan Merevitalisasi Kemitraan Global untuk Pembangunan Berkelanjutan',
                 color:'#19486A',
-                sdgs_icon:'assets/imgs/sdgs17.jpg'
+                sdgc_icon:'assets/imgs/sdgs17.jpg'
             },
             {
                 id:18,
                 title:'KEMITRAAN UNTUK MENCAPAI TUJUAN',
                 subtitle:'Melestarikan dan Memanfaatkan secara Berkelanjutan Sumber Daya Kelautan dan Samudera untuk Pembangunan Berkelanjutan',
                 color:'#19486A',
-                sdgs_icon:'assets/imgs/link.jpg'
+                sdgc_icon:'assets/imgs/link.png'
             }]
 
 export default list;
